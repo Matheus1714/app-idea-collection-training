15b7a50c3e52a7fd067c2d364e7cc9da7ed05fd4
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-04-08 12:36:44 -0300

    logic border radius

---
+++ Border-Radius-Previwers/index.js
@@ -0,0 +1,38 @@
+let submit = document.getElementsByClassName('submit')[0]
+
+const textBorderRadius = () => {
+    let inputValues = document.getElementsByClassName('inputValues')
+    let valuesBorder = ""
+    textCode.textContent = "border-radius: "
+    for(let i = 0; i < inputValues.length; i++){
+        valuesBorder += inputValues[i].value + 'px '
+    }
+    return valuesBorder
+}
+
+const addBoxStyle = (valuesBorder) => {
+    let box = document.getElementsByClassName('box')[0]
+
+    box.style.borderRadius = valuesBorder
+}
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault
+    let textCode = document.getElementsByClassName('textChange')[0]
+    
+    let valuesBorder = textBorderRadius()
+
+    textCode.textContent = ""
+    textCode.textContent += valuesBorder
+    textCode.textContent += ";"
+
+    addBoxStyle(valuesBorder)
+})
+
+let copy = document.getElementsByClassName('copy')[0]
+
+copy.addEventListener('click', (event) => {
+    event.preventDefault
+    document.getElementsByClassName('textCode')[0].select()
+    document.execCommand('copy')
+})
