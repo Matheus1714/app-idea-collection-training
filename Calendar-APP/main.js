6716d91284536ea22f65db9eb32cdcc8ac0a8f9d
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-05-03 17:24:37 -0300

    calendar features

---
+++ Calendar-APP/main.js
@@ -0,0 +1,63 @@
+window.onload = function(){
+    let d = new Date()
+    let month_name = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    let month = d.getMonth()
+    let year = d.getFullYear()
+    let first_date = month_name[month] + " " + 1 + " " + year
+    let tmp = new Date(first_date).toDateString()
+    let first_day = tmp.substring(0, 3)
+    let day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+    let day_no = day_name.indexOf(first_day)
+    let days = new Date(year, month+1, 0).getDate()
+    let calendar = get_calendar(day_no, days)
+    document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year
+    document.getElementById("calendar-dates").appendChild(calendar)
+}
+
+function get_calendar(day_no, days){
+    let table = document.createElement('table')
+    let tr = document.createElement('tr')
+    
+    for(let c=0; c<=6; c++){
+        let td = document.createElement('td')
+        td.innerHTML = "SMTWTFS"[c]
+        tr.appendChild(td)
+    }
+    table.appendChild(tr)
+    
+    tr = document.createElement('tr')
+    let c
+    for(c=0; c<=6; c++){
+        if(c == day_no){
+            break
+        }
+        let td = document.createElement('td')
+        td.innerHTML = ""
+        tr.appendChild(td)
+    }
+    
+    let count = 1
+    for(; c<=6; c++){
+        let td = document.createElement('td')
+        td.innerHTML = count
+        count++
+        tr.appendChild(td)
+    }
+    table.appendChild(tr)
+    
+    for(let r=3; r<=7; r++){
+        tr = document.createElement('tr')
+        for(let c=0; c<=6; c++){
+            if(count > days){
+                table.appendChild(tr)
+                return table
+            }
+            let td = document.createElement('td')
+            td.innerHTML = count
+            count++
+            tr.appendChild(td)
+        }
+        table.appendChild(tr)
+    }
+    return table
+}
